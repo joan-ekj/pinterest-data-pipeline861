4f4e6b3680c74b3f754d2d8567795bd16366a977
Joan Ekajeh <you@example.com>
2024-07-24 20:50:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pinterest Data Pipeline Project 
+## Features 
+This data project forms part of the AiCore Data Engineering Bootcamp.
+
+Pinterest crunches billions of data points every day to decide how to provide more value to their users. In this project, I will create a similar system using the AWS Cloud.
+## Description
+## File Structure
+## Installation
---
+++ user_posting_emulation.py
@@ -0,0 +1,85 @@
+import requests
+from time import sleep
+import random
+from multiprocessing import Process
+import boto3
+import json
+import sqlalchemy
+from sqlalchemy import text
+from datetime import date, datetime
+
+random.seed(100)
+
+
+class AWSDBConnector:
+
+    def __init__(self):
+
+        self.HOST = "pinterestdbreadonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com"
+        self.USER = 'project_user'
+        self.PASSWORD = ':t%;yCY3Yjg'
+        self.DATABASE = 'pinterest_data'
+        self.PORT = 3306
+        
+    def create_db_connector(self):
+        engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
+        return engine
+
+
+new_connector = AWSDBConnector()
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
+
+def send_to_kafka(topic, data):
+        invoke_url = f"https://dob1hsyi8a.execute-api.us-east-1.amazonaws.com/first/topics/{topic}"
+        headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
+        response = requests.post(invoke_url, data=json.dumps(data, default=json_serial), headers=headers)
+        print(f"Sent to Kafka topic {topic}: {response.status_code} - {response.text}")
+        return response
+
+
+def run_infinite_post_data_loop():
+    while True:
+        sleep(random.randrange(0, 2))
+        random_row = random.randint(0, 11000)
+        engine = new_connector.create_db_connector()
+
+        with engine.connect() as connection:
+
+            pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
+            pin_selected_row = connection.execute(pin_string)
+            
+            for row in pin_selected_row:
+                pin_result = dict(row._mapping)
+                send_to_kafka("0affc011d3cf.pin", pin_result)
+
+            geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
+            geo_selected_row = connection.execute(geo_string)
+            
+            for row in geo_selected_row:
+                geo_result = dict(row._mapping)
+                send_to_kafka("0affc011d3cf.geo", geo_result)
+
+            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
+            user_selected_row = connection.execute(user_string)
+            
+            for row in user_selected_row:
+                user_result = dict(row._mapping)
+                send_to_kafka("0affc011d3cf.user", user_result)
+            
+            print(pin_result)
+            print(geo_result)
+            print(user_result)
+
+
+if __name__ == "__main__":
+    run_infinite_post_data_loop()
+    print('Working')
+    
+    
+
+
